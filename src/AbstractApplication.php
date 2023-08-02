e6c0fb4c69e7b6e07c1b280f97b21337e305bc40
Weida <sgenmi@gmail.com>
2023-08-03 00:17:54 +0800

    完善基础类

--- src/AbstractApplication.php
+++ src/AbstractApplication.php
@@ -3,15 +3,17 @@ declare(strict_types=1);
 namespace Weida\Oauth2Core;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use Weida\Oauth2Core\Contract\ConfigInterface;
 use Weida\Oauth2Core\Contract\HttpClientInterface;
 use Weida\Oauth2Core\Contract\Oauth2Interface;
 use Weida\Oauth2Core\Contract\UserInterface;
+use RuntimeException;
 
 /**
- * Author: sgenmi
+ * Author: Weida
  * Date: 2023/7/31 23:29
- * Email: 150560159@qq.com
+ * Email: sgenmi@gmail.com
  */
 abstract class AbstractApplication implements Oauth2Interface
 {
@@ -20,6 +22,7 @@ abstract class AbstractApplication implements Oauth2Interface
     protected HttpClientInterface|ClientInterface $httpClient;
     protected array $scopes=[];
     protected string $scopeSeparator=',';
+    protected string $state="";
 
 
     public function __construct(array $config )
@@ -29,43 +32,40 @@ abstract class AbstractApplication implements Oauth2Interface
 
     /**
      * @return UserInterface
-     * @author Sgenmi
+     * @author Weida
      */
-    public function userFromCode():UserInterface{
-
+    public function userFromCode(string $code):UserInterface{
+        $this->getConfig()->set('code',$code);
+        $tokenArr = $this->tokenFromCode($code);
+        return $this->userFromToken($tokenArr['access_token']);
     }
 
     /**
      * @return UserInterface
-     * @author Sgenmi
+     * @author Weida
      */
-    public function userFromToken(): UserInterface
+    public function userFromToken(string $accessToken): UserInterface
     {
+        $url = $this->getUserInfoUrl($accessToken,$openid);
+        //明天继续
 
     }
 
 
     /**
      * @return string
-     * @author Sgenmi
+     * @author Weida
      */
     public function redirect(): string
     {
-        return $this->getUrl();
+        return $this->getAuthUrl();
     }
 
-    /**
-     * @return array
-     * @author Sgenmi
-     */
-    public function tokenFromCode(): array
-    {
-        return [];
-    }
+
 
     /**
      * @return ConfigInterface
-     * @author Sgenmi
+     * @author Weida
      */
     public function getConfig(): ConfigInterface
     {
@@ -73,8 +73,19 @@ abstract class AbstractApplication implements Oauth2Interface
     }
 
     /**
+     * @param ConfigInterface $config
+     * @return $this
+     * @author Weida
+     */
+    public function setConfig(ConfigInterface $config): static
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    /**
      * @return HttpClientInterface|ClientInterface
-     * @author Sgenmi
+     * @author Weida
      */
     public function getHttpClient(): HttpClientInterface|ClientInterface
     {
@@ -84,18 +95,62 @@ abstract class AbstractApplication implements Oauth2Interface
         return $this->httpClient;
     }
 
+    /**
+     * @param string $state
+     * @return $this
+     * @author Weida
+     */
+    public function withState(string $state):static{
+        return $this->setState($state);
+    }
 
     /**
-     * @return string
+     * @param string $state
+     * @return $this
+     * @author Weida
+     */
+    public function setState(string $state):static{
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * @param array $scopes
+     * @return $this
+     * @author Weida
+     */
+    public function withScopes(array $scopes):static {
+       return $this->setScopes($scopes);
+    }
+
+    /**
+     * @param array $scopes
+     * @return $this
+     * @author Weida
+     */
+    public function setScopes(array $scopes):static {
+        $this->scopes = $scopes;
+        return $this;
+    }
+
+    /**
+     * scope 分割字符，默认逗号
+     * @param string $separator
+     * @return $this
      * @author Sgenmi
      */
-    abstract public function getAuthUrl():string;
+    public function setScopeSeparator(string $separator):static {
+        $this->scopeSeparator = $separator;
+        return $this;
+    }
+
+    abstract protected function getAuthUrl():string;
 
     abstract public function getTokenUrl():string;
 
+    abstract protected function getUserInfoUrl(string $accessToken,string $openid=''):string;
+
     abstract public function getUserByToken():string;
-
-
 
 
 }
